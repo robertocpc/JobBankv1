02d8d160ef36e65df6539a7526c8a5f2590fb58d
robertocpc <robertocpc@github.com>
2017-05-26 21:21:32 -0500

    pprimero

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Esis</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="brading">
+                <h1>ESIS</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="login.php">Log in</a></li>
+                    <li><a href="index.php">Blog</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section id="showcase">
+        <div class="container">
+            <h1>Escuela Profesional de Ingeniería de Informática y Sistemas</h1>
+            <p>Parrafo</p>
+        </div>
+    </section>
+    <section id="newsletter">
+        <div class="container">
+            <h1>¿Qué necesita?</h1>
+            <form action="search.php">
+                <input type="text" placeholder="Introdusca palabra clave">
+                <button>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
