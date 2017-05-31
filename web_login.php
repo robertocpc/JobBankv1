d893e2305ffb433e9606fd654b4c4299bc6739ee
robertocpc <roberto.cpc@hotmail.com>
2017-05-31 14:47:47 -0500

    avance3

---
+++ web_login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Esis</title>
+</head>
+<body>
+    <header>    
+        <div class="container">
+            <div id="branding">  
+                <img src="./img/logo1.png" height="50" width="200">
+            </div>
+            <nav>
+                <div class="contacto">
+                    <p><img src="./img/phone.png"> 583000</p>
+                    <p>anexo: 2005</p>
+                    <p><img src="./img/email.png"> esis@unjbg.edu.pe</p>
+                </div>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="./web_login.php">Log in</a></li>
+                    <li><a href="index.php">Blog</a></li>
+                    <li><a href="./index.php"><img src="./img/fblogo1.png" height="18" width="18"></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div id="showcase">
+        <div class="container">
+            <div class="login access">
+                <form action="./index.php">
+                    <div>
+                        <input type="text" placeholder="Codigo">
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Contraseña">
+                    </div>
+                    <div>
+                        <input type="submit" value="Ingresar">
+                    </div>
+                </form>
+            </div>
+            <div class="login inf">
+                <p>El acceso es solamente para egresados de la Universidad Nacional Jorge Basadre Grohman que pertenecen a la Escuela Profeisonal de Ingeniería de Informática y Sistemas</p>
+            </div>
+        </div>
+    </div>
+    <div class="moveimage">
+    <img src="./img/opacity1.png" height="50" width="100%">
+    </div>
+</body>
+</html>
