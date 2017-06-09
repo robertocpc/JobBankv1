c3eedbcc67635492cc97747995b4dcf61ee671d4
robertocpc <roberto.cpc@hotmail.com>
2017-06-09 11:01:42 -0500

    avance4

--- session.php
+++ session.php
@@ -1,8 +1,50 @@
-<?php 
-include './header.php';
-?>
-        <div class="moveimage">
-        <p>hols</p>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Esis</title>
+    </head>
+    <body>
+        <header>    
+            <div class="container">
+                <div id="branding">  
+                    <img src="./img/logo_50.png">
+                </div>
+                <nav>
+                    <div class="contacto">
+                        <p><img src="./img/phone.png" height="13" width="13"> 583000</p>
+                        <p>anexo: 2005</p>
+                        <p><img src="./img/email.png"  height="13" width="13"> esis@unjbg.edu.pe</p>
+                    </div>
+                    <ul>
+                        <div class="indices marco">
+                            <li><a href="index.php">Inicio</a></li>
+                            <li><a href="./web_login.php">Log in</a></li>
+                            <li><a href="./session.php">Blog</a></li>
+                        </div>
+                        <div class="indices marco_logo">
+                            <li><a href="./index.php"><img src="./img/fblogo1.png" height="18" width="18"></a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="container">
+            <div class="formulario">
+                <div class="tab_izquierda">
+                    <a class="efx_button1">Información Personal</a>
+                    <a class="efx_button1">Capacitaciones</a>
+                    <a class="efx_button1">Especializaciones</a>
+                    <a class="efx_button1">Bolsa de Trabajo</a>
+                </div>
+                <div class="tab_derecha">
+                    <div class="tab_panel">
+                        <p>Primer Parrafo</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
