ca385771f1ca8e6af6eadc2caf67be814c422a41
robertocpc <robertocpc@github.com>
2017-06-10 22:08:48 -0500

    avance5

--- session.php
+++ session.php
@@ -1,43 +1,21 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="./css/style.css">
-        <title>Esis</title>
-    </head>
-    <body>
-        <header>    
-            <div class="container">
-                <div id="branding">  
-                    <img src="./img/logo_50.png">
-                </div>
-                <nav>
-                    <div class="contacto">
-                        <p><img src="./img/phone.png" height="13" width="13"> 583000</p>
-                        <p>anexo: 2005</p>
-                        <p><img src="./img/email.png"  height="13" width="13"> esis@unjbg.edu.pe</p>
-                    </div>
-                    <ul>
-                        <div class="indices marco">
-                            <li><a href="index.php">Inicio</a></li>
-                            <li><a href="./web_login.php">Log in</a></li>
-                            <li><a href="./session.php">Blog</a></li>
-                        </div>
-                        <div class="indices marco_logo">
-                            <li><a href="./index.php"><img src="./img/fblogo1.png" height="18" width="18"></a></li>
-                        </div>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+<?php
+include './header.php';
+?>
         <div class="container">
             <div class="formulario">
+                <!--nav class="tab_izquierda">
+                    <ul>
+                    <li><a href="./panel_inf.php" class="efx_button1">Información Personal</a></li>
+                    <li><a href="./panel_cap.php" class="efx_button1">Capacitaciones</a></li>
+                    <li><a href="./panel_esp.php" class="efx_button1">Especializaciones</a></li>
+                    <li><a href="./panel_bdt.php" class="efx_button1">Bolsa de Trabajo</a></li>
+                    </ul>
+                </nav-->
                 <div class="tab_izquierda">
-                    <a class="efx_button1">Información Personal</a>
-                    <a class="efx_button1">Capacitaciones</a>
-                    <a class="efx_button1">Especializaciones</a>
-                    <a class="efx_button1">Bolsa de Trabajo</a>
+                    <button onclick="location.href = './index.php';" class="efex_button1">Información Personal</button><br>
+                    <button onclick="location.href = './index.php';" class="efex_button1">Capacitaciones</button><br>
+                    <button onclick="location.href = './index.php';" class="efex_button1">Especializaciones</button><br>
+                    <button onclick="location.href = './index.php';" class="efex_button1">Bolsa de Trabajo</button>
                 </div>
                 <div class="tab_derecha">
                     <div class="tab_panel">
