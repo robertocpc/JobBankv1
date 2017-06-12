e8e40b1f45df22b1a80a12b7acaadccc4984f61e
robertocpc <robertocpc@github.com>
2017-06-11 23:50:38 -0500

    avance6

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '4kegezvc';
+$db = 'ISWork';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
--- session.php
+++ session.php
@@ -2,6 +2,21 @@
 include './header.php';
 ?>
         <div class="container">
+            <!--div class="panel-inicio">
+                <center><div class="izquierda"><img src="./img/profimage.png" height="165"></div>
+                <label class="font">Roberto Carlos Pongo Condori</label><br>
+                <label>Front-end Developer</label>
+                </center>
+                <ul>
+                    <li><a href="./account/profile.php"><img src="./imglogo/user.png"></a></li>
+                    <li><a href="./account/certificate.php"><img src="./imglogo/certificate.png"></a></li>
+                    <li><a href="./account/special.php"><img src="./imglogo/school.png"></a></li>
+                    <li><a href="./account/work.php"><img src="./imglogo/briefcase.png"></a></li>
+                </ul>
+            </div-->
+
+
+
             <div class="formulario">
                 <!--nav class="tab_izquierda">
                     <ul>
@@ -19,7 +34,31 @@ include './header.php';
                 </div>
                 <div class="tab_derecha">
                     <div class="tab_panel">
-                        <p>Primer Parrafo</p>
+                        <center>
+                            <img src="./img/profimage.png" height="160px">
+                            <form method="POST" enctype="multipart/form-data">                           
+                                    <label class="buttonefex1"><input class="file" type="file" title="files" name="image">Custom</label>
+                                    <input class="buttonefex1" type="submit" name="submit" value="upload">
+                            </form>
+                        </center>
+                    </div>
+                    <div class="tab_panel padd">
+                        <div class="tabizq">
+                            <label class="tagg">Nombre:</label>
+                            <label class="tagtext">Roberto Carlos</label>
+                        </div>
+                        <div class="tabder">    
+                            <label class="tagg">Apellido:</label>
+                            <label class="tagtext">Pongo Condori</label> 
+                        </div>
+                        <div class="tabizq">
+                            <label class="tagg">Email:</label>
+                            <label class="tagtext">roberto.crlp@hotmail.com</label>
+                        </div>
+                        <div class="tabder">
+                            <label class="tagg">Email:</label>
+                            <label class="tagtext">roberto.crlp@hotmail.com</label>
+                        </div>
                     </div>
                 </div>
             </div>
