59118e2a62c241ad73e19274d60c7284b5f8d0fe
robertocpc <robertocpc@github.com>
2017-07-01 16:04:49 -0500

    sabado2

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ session_start();
 $_SESSION['windows']=5;
 include './header.php';
 ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <Script src="./jquery.js"></Script>
     <div id="showcase">
         <div class="container sub1">
+            <div class="search" id="search">
+            
 
             <h1 id="titulo">BOLSA DE TRABAJO</h1>
             
@@ -13,17 +17,38 @@ include './header.php';
             
 
             <center><h3>Universidad Nacional Jorge Basadre Grohmann</h3></center>
-            <p></p>
+            <p></p><br><br><br><br><br><br>
+            </div>
         </div>
         <div id="newsletter">
             <div class="container">
                 <h1>¿Busca algún profesional..?</h1>
-                <form action="search.php">
-                    <input type="search" placeholder="Introdusca palabra clave">
-                    <input type="submit" class="button_1" value="Buscar"></input>
+                <form action="search.php" name="buscar_form" id="buscar_form">
+                    <input name="buscar" id="buscar" type="search" placeholder="Introdusca palabra clave">
                 </form>
             </div>
         </div>
+
+
     </div>
+    <script type="text/javascript">
+    
+   $(function(){
+    $('#buscaar').focus();
+    $('#buscar_form').submit(function(e){
+        e.preventDefault();
+    })
+
+    $('#buscar').keyup(function(){
+        var envio=$('#buscar').val();
+
+        $('#search').html('<h1>Resultados de la búsqueda</h1>');
+        //$('#resultados').html('<h2><img src="img/load.gif" width="20">Loading... </h2>');
+
+    })
+})
+
+    
+    </script>
 </body>
 </html>
